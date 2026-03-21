3217d41dd64443c1374ccd45a5477980999baf70
Mabel <59884023+GamTam@users.noreply.github.com>
2026-03-21 18:18:41 -0400

    Drawing text on screen

--- Main.cpp
+++ Main.cpp
@@ -11,11 +11,23 @@ using namespace sf;
 
 int main()
 {
-    Window window(VideoMode({ 1280, 720 }), "Pack Mann");
+    RenderWindow window(VideoMode({ 1280, 720 }), "Pack Mann");
+    Font font("assets/Subert Gaming.otf");
+    Clock clock;
+
+    Text text(font);
+    text.setCharacterSize(24);
+    text.setFillColor(Color::White);
+    text.setString("me when im #Gaming");
+    text.setPosition(Vector2f(0, 0));
+
+    float xPos = 0;
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
+        float delta = clock.restart().asSeconds();
+
         // check all the window's events that were triggered since the last iteration of the loop
         while (const optional event = window.pollEvent())
         {
@@ -23,5 +35,12 @@ int main()
             if (event->is<Event::Closed>())
                 window.close();
         }
+        xPos += delta * 40;
+
+        text.setPosition(Vector2f(xPos, xPos));
+
+        window.clear(Color::Black);
+        window.draw(text);
+        window.display();
     }
 }
