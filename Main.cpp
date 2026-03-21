eab621ff61b90035a569b664eb0e9180c63d224f
ChaosRulerofAll <ilya@soukoreff.com>
2026-03-21 17:40:37 -0400

    Added libraries and namespaces

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,26 @@
 #include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+
 
 int main()
 {
-    sf::Window window(sf::VideoMode({ 800, 600 }), "My window");
+    Window window(VideoMode({ 800, 600 }), "My window");
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
-        while (const std::optional event = window.pollEvent())
+        while (const optional event = window.pollEvent())
         {
             // "close requested" event: we close the window
-            if (event->is<sf::Event::Closed>())
+            if (event->is<Event::Closed>())
                 window.close();
         }
     }
